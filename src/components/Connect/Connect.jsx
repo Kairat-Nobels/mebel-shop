f3fb5f4f3ac2f1d618827df2c93dcf1da913b684
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2026-03-11 03:58:37 +0600

    done

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -9,7 +9,7 @@ const Connect = () => {
             <h3 data-aos="fade-up">Наши социальные сети!</h3>
             <div className="connections">
                 <HomeConnectItem logo={<AiFillFacebook />} link="https://www.facebook.com/" />
-                <HomeConnectItem logo={<AiOutlineWhatsApp />} link="https://wa.me/996550976476" />
+                <HomeConnectItem logo={<AiOutlineWhatsApp />} link="https://wa.me/996555230419" />
                 <HomeConnectItem logo={<AiOutlineGoogle />} link="https://www.google.com/" />
                 <HomeConnectItem logo={<AiFillInstagram />} link="https://www.instagram.com/" />
             </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
     return (
         <footer style={{ background: "#3a3a3a" }} className='d-flex flex-column flex-sm-row justify-content-between page-container p-5'>
             <div className="footer-left pt-2">
-                <span className='text-white'>Copyright © 2025 Furniture Store</span>
+                <span className='text-white'>Copyright © 2026 Furniture Store</span>
             </div>
             <div className="footer-right pt-2">
                 <span className='pe-1 text-white'>Made by</span>
-                <span className='fw-bold' style={{ color: "#78909c", cursor: "pointer" }} onClick={() => { window.open("https://www.instagram.com/aitukv?igsh=MTJtMHZvZ3B2ZzVzcw%3D%3D&utm_source=qr ") }}>Developer</span>
+                <span className='fw-bold' style={{ color: "#78909c", cursor: "pointer" }} onClick={() => { window.open("https://www.instagram.com/ ") }}>Developer</span>
             </div>
         </footer>
     )
